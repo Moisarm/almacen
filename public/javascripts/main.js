dd201f0bfe3d764f753025b35cc6d29ff5984551
Moisarm <v.30247986.mr@gmail.com>
2024-08-10 08:00:49 -0400

    status

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,5 @@
 
-/*Codigos que le dan funcionalidad a las pantallas de Productos */
-var idActualizarCategoriaGlobal = ''
-var idActualizarProductoGlobal = ''
 
-
-function changeIdActualizar(id, nombreTabla){
-    console.log(`ESTO ES EL ID NUEVO: ${id} | nombreTabla: ${nombreTabla}`)
-
-    if(nombreTabla==="producto"){
-
-        idActualizarProductoGlobal = id
-
-    }else{
-
-        idActualizarCategoriaGlobal = id
-    }
-}
 
 
 
